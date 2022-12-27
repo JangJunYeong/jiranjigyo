cdef536ba085d9c415e6031f8941377ebb54027c
Cho-Hyun-Ji <104757451+Cho-Hyun-Ji@users.noreply.github.com>
2022-12-27 14:56:14 +0900

    update complaint

--- lib/complaint.dart
+++ lib/complaint.dart
@@ -1,20 +1,66 @@
-// import 'package:contact/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:jiranjigyo/theme.dart';
 
 
-class MyComplaint extends StatelessWidget {
+class ComplaintButton extends StatelessWidget {
+  const ComplaintButton({
+    Key? key,
+    required this.text,
+    this.checked = false,
+    required this.onPressed,
+  }) : super(key: key);
 
+  final String text;
+  final bool checked;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: checked
+          ? const Color(0xFFC4C6D6)
+          : Theme.of(context).colorScheme.secondaryContainer,
+      borderRadius: BorderRadius.circular(10.0),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10.0),
+        onTap: onPressed,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Center(
+            child: Text(text,
+              style: const TextStyle(fontSize: 18.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class MyComplaint extends StatefulWidget {
   const MyComplaint({Key? key}) : super(key: key);
 
+  @override
+  State<MyComplaint> createState() => _MyComplaintState();
+}
 
+class _MyComplaintState extends State<MyComplaint> {
+  int? checkedIndex;
 
   @override
   Widget build(BuildContext context) {
     final textController = TextEditingController();
+    List<String> contents = [
+      '질문과 답변', '고장 신고', '이용 신고', '분실 신고',
+    ];
+
     return MaterialApp(
         theme: ThemeData(
           useMaterial3: true,
-          // colorScheme: lightColorScheme,
+          colorScheme: lightColorScheme,
         ),
         home: Scaffold(
             body: Padding(
@@ -27,34 +73,26 @@ class MyComplaint extends StatelessWidget {
                     children: [
                       Text('민원 내용을 선택해주세요.'),
                       SizedBox(height: 30),
-                      GridView(
+                      GridView.builder(
                         shrinkWrap: true,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        itemCount: contents.length,
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
                           childAspectRatio: 2, //item 의 가로 1, 세로 2 의 비율
                           mainAxisSpacing: 30, //수평 Padding
                           crossAxisSpacing: 30, //수직 Padding
                         ),
-
-                        children: [
-
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text('질문과 답변'),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text('고장 신고'),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text('이용 신고'),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text('분실 신고'),
-                          ),
-                        ],
+                        itemBuilder: (context, index) {
+                          return ComplaintButton(
+                            text: contents[index],
+                            checked: checkedIndex == index,
+                            onPressed: () {
+                              setState(() {
+                                checkedIndex = index;
+                              });
+                            },
+                          );
+                        },
                       ),
                       SizedBox(height: 60),
                       Text('상세 내용'),
@@ -71,7 +109,12 @@ class MyComplaint extends StatelessWidget {
                   ),
                   SizedBox(height: 30),
                   ElevatedButton(
+
                     child: const Text('확인'),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(120, 40),
+                      textStyle: const TextStyle(fontSize: 18)
+                    ),
                     onPressed: () {
                       Navigator.pop(context);
                     },
