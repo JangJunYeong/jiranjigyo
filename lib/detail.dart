7fc90f72b74fb668f68fcf0aed46c12dd5d01969
JangJunYeong <wkdwnsdud25@gmail.com>
2022-12-28 16:55:50 +0900

    세부사항 입력 칸 리스트 출력

--- lib/detail.dart
+++ lib/detail.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import './widget/bottomtabbar.dart';
 import 'package:jiranjigyo/widget/appbar.dart';
 
+var stepList = List.empty(growable: true);
+var mancount = 0;
+
 final List<String> _filters = <String>[];
-int count = 0;
 
 class DetailPage extends StatefulWidget{
   const DetailPage({Key? key}) : super(key: key);
@@ -13,8 +15,6 @@ class DetailPage extends StatefulWidget{
 }
 
 class _DetailPageState extends State<DetailPage> {
-  int index = 0;
-
   @override
   void initState(){
     super.initState();
@@ -22,7 +22,7 @@ class _DetailPageState extends State<DetailPage> {
   @override
   void dispose() {
     _filters.clear();
-    count = 0;
+    mancount = 0;
     super.dispose();
   }
   void refresh() {
@@ -37,8 +37,162 @@ class _DetailPageState extends State<DetailPage> {
         child: Scaffold(
           appBar: AppBarWidget(AppBar(), "예약 세부사항"),
           bottomNavigationBar: const BottomTabBar(0),
-        ),
-      ),
+          body:
+          ListView(scrollDirection: Axis.vertical, shrinkWrap: true, children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                height: 10,
+                ),
+                const Divider(
+                  indent: 20,
+                  endIndent: 20,
+                  color: Colors.black,
+                  thickness: 1.0,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.875,
+                  height: 400,
+                  padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+                  alignment: Alignment.topCenter,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.black)),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 300,
+                        child: ListView(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          children: getContent(),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+              ]
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: MyTextInput(notifyParent: refresh),
+            ),
+          ]),
+        )
+      )
     );
   }
 }
+
+class MyTextInput extends StatefulWidget {
+  const MyTextInput({super.key, required this.notifyParent});
+  final Function() notifyParent;
+
+  @override
+  MyTextInputState createState() => MyTextInputState();
+}
+
+class MyTextInputState extends State<MyTextInput> {
+  final myTitleController = TextEditingController();
+  final myTimeController = TextEditingController();
+  final myFileNameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text('제목'),
+              TextField(
+                controller: myTitleController,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text("시간"),
+              TextField(
+                controller: myTimeController,
+              ),
+              Row(
+                children: <Widget>[
+                  ElevatedButton(
+                    child: const Icon(Icons.done),
+                    onPressed: () {
+                      stepList.add([myTimeController.text, myTitleController.text]);
+                      widget.notifyParent();
+                    },
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.0375,
+                  ),
+                  ElevatedButton(
+                    child: const Icon(Icons.close),
+                    onPressed: () {
+                      stepList = List.empty(growable: true);
+                      widget.notifyParent();
+                    },
+                  ),
+                ],
+              )
+            ]));
+  }
+}
+
+List<Widget> getContent() {
+  mancount = 1;
+  List<Widget> tiles = [];
+  if (stepList.isEmpty) {
+    tiles.add(const Text("empty"));
+  } else {
+    for (var element in stepList) {
+      tiles.add(StepTile(Step(element[0], element[1])));
+      mancount++;
+    }
+  }
+  return tiles;
+}
+
+class Step {
+  Step(this.title, this.time);
+
+  String title;
+  String time;
+}
+
+class StepTile extends StatelessWidget {
+  StepTile(this._step, {super.key});
+
+  final Step _step;
+  final ind = mancount.toString();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(_step.time),
+          leading: Text(ind),
+          subtitle: Text(
+            _step.title,
+            textAlign: TextAlign.right,
+          ),
+        ),
+        const Divider(
+          color: Colors.black,
+          thickness: 1.0,
+        ),
+      ],
+    );
+  }
+}
+
