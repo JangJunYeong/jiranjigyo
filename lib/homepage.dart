b583dacbf8e7286c6d5d6e810b8ae1627eaaac50
승민 김 <ksm13727@gmail.com>
2022-12-29 14:32:24 +0900

    homepage m

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,7 +6,6 @@ import 'widget/loginCard.dart';
 
 import 'reservation.dart';
 import 'complaint.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 var stepList = List.empty(growable: true);
 var index = 1;
@@ -32,271 +31,249 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const SizedBox(
-              height: 50,
-            ),
-
-            const LoginCard(),
-
-            const SizedBox(
-              height: 15,
-            ),
-            const Divider(
-              indent: 20,
-              endIndent: 20,
-              color: Colors.black,
-              thickness: 1.0,
-            ),
-            Container(
-                width: MediaQuery.of(context).size.width * 0.9,
-                height: 30,
-                padding: const EdgeInsets.fromLTRB(15.0, 0, 15.0, 0),
-                alignment: Alignment.centerLeft,
-                decoration: BoxDecoration(
-                    color: lightColorScheme.primaryContainer, // 배경 색
-                    border: Border.all(
-                        width: 1, color: Colors.transparent), // 외곽선 투명
-                    borderRadius:
-                        const BorderRadius.all(Radius.circular(8.0)) // 곡률
-                    ),
-                child: Row(
-                  children: [
-                    const Text(
-                      "대충 공지",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.575,
-                    ),
-                    TextButton(
-                        onPressed: () => {
-                          launchUrl(
-                            Uri.parse('https://computer.cnu.ac.kr/computer/index.do')
-                          )
-                        },
-                        child: Text("더보기")
-                    )
-                  ],
-                )),
-            const Divider(
-              indent: 20,
-              endIndent: 20,
-              color: Colors.black,
-              thickness: 1.0,
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.875,
-              height: 400,
-              padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
-              alignment: Alignment.topCenter,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(
+            height: 10,
+          ),
+          const LoginCard(),
+          const SizedBox(
+            height: 5,
+          ),
+          const Divider(
+            indent: 20,
+            endIndent: 20,
+            color: Colors.black,
+            thickness: 1.0,
+          ),
+          Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 30,
+              padding: const EdgeInsets.fromLTRB(15.0, 0, 15.0, 0),
+              alignment: Alignment.centerLeft,
               decoration: BoxDecoration(
-                  color: lightColorScheme.secondaryContainer, // 배경 색
+                  color: lightColorScheme.primaryContainer, // 배경 색
                   border:
                       Border.all(width: 1, color: Colors.transparent), // 외곽선 투명
                   borderRadius:
                       const BorderRadius.all(Radius.circular(8.0)) // 곡률
                   ),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "나의 예약 현황",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 30),
-                      ),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.25,
-                      ),
-                      const Text(
-                        "더보기",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
+              child: Row(
+                children: [
+                  const Text(
+                    "대충 공지",
+                    style: TextStyle(color: Colors.white),
                   ),
-                  Card(
-                    elevation: 3,
-                    color: Theme.of(context).colorScheme.secondaryContainer,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(5, 10, 15, 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          ListTile(
-                            title: const Text(
-                              style: TextStyle(fontSize: 20),
-                              '12월28일(목)',
-                            ),
-                            subtitle: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                    style: TextStyle(fontSize: 17),
-                                    '14:00~16:00'),
-                                ElevatedButton(
-                                  child: const Text(
-                                      style: TextStyle(
-                                          color:
-                                              Color.fromARGB(255, 56, 55, 55)),
-                                      '인증하기'),
-                                  onPressed: () {/* ... */},
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.575,
                   ),
-                  const SizedBox(
-                    height: 10,
+                  const Text(
+                    "더보기",
+                    style: TextStyle(color: Colors.white),
                   ),
                 ],
+              )),
+          const Divider(
+            indent: 20,
+            endIndent: 20,
+            color: Colors.black,
+            thickness: 1.0,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.875,
+            height: 400,
+            padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+            alignment: Alignment.topCenter,
+            decoration: BoxDecoration(
+                color: lightColorScheme.secondaryContainer, // 배경 색
+                border:
+                    Border.all(width: 1, color: Colors.transparent), // 외곽선 투명
+                borderRadius: const BorderRadius.all(Radius.circular(8.0)) // 곡률
+                ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "나의 예약 현황",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.25,
+                    ),
+                    const Text(
+                      "더보기",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+                Card(
+                  elevation: 3,
+                  color: Theme.of(context).colorScheme.secondaryContainer,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(5, 10, 15, 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          title: const Text(
+                            style: TextStyle(fontSize: 20),
+                            '12월28일(목)',
+                          ),
+                          subtitle: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                  style: TextStyle(fontSize: 17),
+                                  '14:00~16:00'),
+                              ElevatedButton(
+                                child: const Text(
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 56, 55, 55)),
+                                    '인증하기'),
+                                onPressed: () {/* ... */},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shadowColor: Colors.grey,
+                  elevation: 6,
+                  shape: (RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0))),
+                  minimumSize:
+                      Size(MediaQuery.of(context).size.width * 0.375, 60),
+                  backgroundColor: lightColorScheme.primaryContainer,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ReservationPage()));
+                },
+                child: const Text(
+                  "예약",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.grey,
-                    elevation: 6,
-                    shape: (RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0))),
-                    minimumSize:
-                        Size(MediaQuery.of(context).size.width * 0.375, 60),
-                    backgroundColor: lightColorScheme.primaryContainer,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ReservationPage()));
-                  },
-                  child: const Text(
-                    "예약",
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.125,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shadowColor: Colors.grey,
+                  elevation: 6,
+                  shape: (RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0))),
+                  minimumSize:
+                      Size(MediaQuery.of(context).size.width * 0.375, 60),
+                  backgroundColor: lightColorScheme.primaryContainer,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ConfirmPage()));
+                },
+                child: const Text(
+                  "예약 확인",
+                  style: TextStyle(
+                    fontSize: 25,
                   ),
                 ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.125,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shadowColor: Colors.grey,
+                  elevation: 6,
+                  shape: (RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0))),
+                  minimumSize:
+                      Size(MediaQuery.of(context).size.width * 0.375, 60),
+                  backgroundColor: lightColorScheme.primaryContainer,
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.grey,
-                    elevation: 6,
-                    shape: (RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0))),
-                    minimumSize:
-                        Size(MediaQuery.of(context).size.width * 0.375, 60),
-                    backgroundColor: lightColorScheme.primaryContainer,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ConfirmPage()));
-                  },
-                  child: const Text(
-                    "예약 확인",
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.grey,
-                    elevation: 6,
-                    shape: (RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0))),
-                    minimumSize:
-                        Size(MediaQuery.of(context).size.width * 0.375, 60),
-                    backgroundColor: lightColorScheme.primaryContainer,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const MyComplaint()));
-                  },
-                  child: const Text(
-                    "민원",
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MyComplaint()));
+                },
+                child: const Text(
+                  "민원",
+                  style: TextStyle(
+                    fontSize: 25,
                   ),
                 ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.125,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.125,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shadowColor: Colors.grey,
+                  elevation: 6,
+                  shape: (RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0))),
+                  minimumSize:
+                      Size(MediaQuery.of(context).size.width * 0.375, 60),
+                  backgroundColor: lightColorScheme.primaryContainer,
                 ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shadowColor: Colors.grey,
-                    elevation: 6,
-                    shape: (RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0))),
-                    minimumSize:
-                        Size(MediaQuery.of(context).size.width * 0.375, 60),
-                    backgroundColor: lightColorScheme.primaryContainer,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const CheckOutPage()));
-                  },
-                  child: const Text(
-                    "퇴실",
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const CheckOutPage()));
+                },
+                child: const Text(
+                  "퇴실",
+                  style: TextStyle(
+                    fontSize: 25,
                   ),
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-          ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
       ),
     );
   }
-}
-
-List<Widget> getContent() {
-  index = 1;
-  List<Widget> tiles = [];
-  if (stepList.isEmpty) {
-    tiles.add(const Text("empty"));
-  } else {
-    for (var element in stepList) {
-      tiles.add(StepTile(Step(element[0], element[1])));
-      index++;
-    }
-  }
-  return tiles;
 }
 
 class Step {
